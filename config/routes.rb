982cd4522926c45368e0c637d2bbaabe508c8a68
Christian Carlo Cruz <christiancruz.carlo@gmail.com>
2018-03-27 16:38:32 +0000

    Added game controller with index and show methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'games/index'
+
+  get 'games/show'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
