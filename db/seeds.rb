1f4b5c1e3a3276133138f1f3a3ae5202acbe7968
Christian Carlo Cruz <christiancruz.carlo@gmail.com>
2018-03-27 06:57:59 +0000

    Added seed.db and csv files for game

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+Genre.delete_all
+Game.delete_all
+
+csv_wards = File.read(Rails.root.join('db', 'game.csv'))
+csv = CSV.parse(csv_wards, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+
+
+    t = Genre.new
+    t.name = row[3]
+    t.save
+
+
+    x = Game.new
+    x.name = row[0]
+    x.year = row[2]
+    x.publisher = row[4]
+    x.developer = row[5]
+    x.rating = row[6]
+    x.genre_id = t.id
+    x.save
+    puts "#{x.name}.#{t.name}.........  saved"
+end
