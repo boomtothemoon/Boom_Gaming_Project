a4cb912d9188e65e56a66e677d55a664b53fd911
Christian Carlo Cruz <christiancruz.carlo@gmail.com>
2018-03-27 05:40:12 +0000

    Added Game model

---
+++ db/migrate/20180327053933_create_games.rb
@@ -0,0 +1,14 @@
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.int :year
+      t.string :publisher
+      t.string :developer
+      t.string :rating
+      t.references :genre, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
